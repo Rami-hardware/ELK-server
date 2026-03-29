c5343581b2f7a6023dd62689db952f785bd158ff
Rami-hardware <killx51@hotmail.com>
2026-03-29 15:54:36 +0300

    Value for undeclared variable

--- terraform/modules/variables.tf
+++ terraform/modules/variables.tf
@@ -1,2 +1,11 @@
 variable "vpc_id" {}
 variable "vswitch_id" {}
+variable "alicloud_access_key" {
+  description = "Alibaba Cloud access key"
+  sensitive   = true
+}
+
+variable "alicloud_secret_key" {
+  description = "Alibaba Cloud secret key"
+  sensitive   = true
+}
